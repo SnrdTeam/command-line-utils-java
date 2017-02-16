6a0acec0ff343f123f77146468c5e7943054e9bc
Евгений Ажогин <azhogin@adeptik.com>
2017-02-16 12:28:25 +0400

    initial commit

---
+++ src/main/java/com/adeptik/extensions/PrintStreamExtensions.java
@@ -0,0 +1,48 @@
+package com.adeptik.extensions;
+
+import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+
+public final class PrintStreamExtensions {
+
+    /**
+     * Вывод в консоль информации об ошибке
+     *
+     * @param printStream PrintStream, в который необходимо вывести данные
+     * @param throwable   Исключение, описывающее ошибку
+     * @param causeIndent Отступ для вложенных исключений
+     */
+    public static void print(PrintStream printStream,
+                             Throwable throwable,
+                             String causeIndent) {
+
+        if (printStream == null)
+            throw new NullPointerException("printStream");
+        if (throwable == null)
+            throw new NullPointerException("throwable");
+
+        if (throwable instanceof InvocationTargetException && throwable.getCause() != null)
+            print(printStream, throwable.getCause(), causeIndent);
+        else {
+            String indent = "";
+            for (Throwable e = throwable; e != null; e = e.getCause()) {
+                printStream.print(indent);
+                printStream.println(e.getMessage());
+                indent += causeIndent;
+            }
+        }
+    }
+
+    /**
+     * Вывод в консоль информации об ошибке.
+     * В качестве строки отступа для вложенных исключений используется " ".
+     *
+     * @param printStream PrintStream, в который необходимо вывести данные
+     * @param throwable   Исключение, описывающее ошибку
+     */
+    public static void print(PrintStream printStream,
+                             Throwable throwable) {
+
+        print(printStream, throwable, " ");
+    }
+}
